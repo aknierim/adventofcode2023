d4e97b19823b39d9c49589ce5c9891ca914fadcb
Anno Knierim <anno.knierim@tu-dortmund.de>
2023-11-29 18:43:43 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 pub mod input;
 pub use crate::input::input::get_input;
 
-fn main() {
-    let input = get_input(1, 2022).expect("failed");
-    println!("{}", input);
+fn main() -> std::io::Result<()> {
+    let input = get_input(1, 2022).expect("Failed to fetch input data!");
+    // println!("{}", input);
+
+    Ok(())
 }
