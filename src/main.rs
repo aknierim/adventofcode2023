05a03e363d3ae6ac5128281561818a3989b94977
Anno Knierim <anno.knierim@tu-dortmund.de>
2023-12-01 15:19:54 +0100

    Delete main.rs

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-pub mod input;
-pub use crate::input::input::get_input;
-
-fn main() -> std::io::Result<()> {
-    let input = get_input(1, 2022).expect("Failed to fetch input data!");
-    println!("{}", input);
-
-    Ok(())
-}
